5eab9cb1b50be0177cfa468c43ae39e46039f81d
James G. E. McIlhargey <jmcilhargey@bloomberg.net>
2021-02-09 09:44:41 -0500

    Updated from the mod portal, with updates from qwerter96 and DarkNova

--- control.lua
+++ control.lua
@@ -1,5 +1,5 @@
 script.on_event(defines.events.on_built_entity, function(event)
-  if string.match(event.created_entity.name, "pipe-") and 
+  if string.match(event.created_entity.name, "pipe-") and
      (   string.match(event.created_entity.name, "elbow")
       or string.match(event.created_entity.name, "junction")
       or string.match(event.created_entity.name, "straight")) then
